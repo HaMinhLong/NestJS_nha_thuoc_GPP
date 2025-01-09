1ef11e3ec574a898199b08038db5d6b5204d27d7
Hà Minh Long <longhm@i.lisod.vn>
2025-01-09 09:59:12 +0700

    Update jest config

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserRepository } from './user.repository'; // Thêm import UserRepository
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserRepository),
+          useValue: { find: jest.fn(), save: jest.fn() }, // Mock các phương thức cần thiết của UserRepository
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
